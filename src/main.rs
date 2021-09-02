6b864e54edf24df57429fcbe74de1495fab3c5b2
Snehit Sah <snehitsah@protonmail.com>
2021-09-02 07:15:35 +0530

    F: Can build

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::{
     path::PathBuf,
 };
 
+use walkdir::WalkDir;
+
 fn create_project(path: PathBuf) {
     // println!("{:?}", path);
     let path_string = String::from(path.to_str().unwrap());
@@ -21,10 +23,37 @@ fn create_project(path: PathBuf) {
     }
 }
 
+fn build_project() {
+    let working_directory = String::from(env::current_dir().unwrap().to_str().unwrap());
+    fs::create_dir_all(format!("{}/target", working_directory)).unwrap();
+
+    let folders: Vec<&str> = vec!["assets", "_pages", "styles"];
+
+    for folder in folders {
+        for entry in WalkDir::new(format!("{}/{}", working_directory, folder)) {
+            let entry = entry.unwrap();
+            let entry_discovered = &entry.path().to_str().unwrap()[working_directory.len()..];
+
+            match fs::copy(
+                format!("{}", entry.path().display()),
+                format!("{}/target/{}", working_directory, folder),
+            ) {
+                Ok(_) => {}
+                Err(_) => {
+                    fs::create_dir_all(format!(
+                        "{}/target/{}",
+                        working_directory, entry_discovered
+                    ))
+                    .unwrap();
+                }
+            };
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("{:?}", args);
     if args.len() > 1 {
         if args[1] == "init" {
             create_project(env::current_dir().unwrap());
@@ -41,6 +70,8 @@ fn main() {
             } else {
                 println!("[ERROR] Please provide a name for this project.")
             };
+        } else if args[1] == "build" {
+            build_project();
         } else {
             println!("[ERROR] Subcommand \"{}\" not recognized.", args[1]);
         }
