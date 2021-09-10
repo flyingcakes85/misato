f980332a3b919ad4c89ef6e4cb86fee7c0b7d178
Snehit Sah <snehitsah@protonmail.com>
2021-09-10 19:00:31 +0530

    Add Test

--- src/build/layout_utils.rs
+++ src/build/layout_utils.rs
@@ -8,9 +8,12 @@ use std::{
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
+    use std::{
+        fs::{self, create_dir_all},
+        path::PathBuf,
+    };
 
-    use crate::build::layout_utils::names_from_path;
+    use crate::build::layout_utils::{available_layouts, names_from_path};
 
     #[test]
     /// Tests names_from_path()
@@ -37,6 +40,48 @@ mod tests {
         ];
 
         assert_eq!(names_from_path(path_list), expected_file_list);
+    }
+
+    #[test]
+    /// Tests available_layouts()
+    fn check_layout_detection() {
+        let base_path: PathBuf = [r".", "test_cache", "_layouts"].iter().collect();
+        create_dir_all(&base_path).unwrap();
+
+        let layouts: Vec<String> = vec![
+            "about".to_string(),
+            "content_page".to_string(),
+            "home".to_string(),
+            "post".to_string(),
+            "profile".to_string(),
+        ];
+
+        let mut expected_path_list: Vec<String> = Vec::new();
+
+        // create the layout files
+        for l in layouts {
+            let mut layout_path = base_path.clone();
+            layout_path.push(l);
+            layout_path.set_extension(".html");
+
+            // also build the expected path list simultaneously
+            expected_path_list.push(layout_path.clone().to_str().unwrap().to_string());
+
+            fs::write(layout_path, "").unwrap();
+        }
+
+        // convect PathBuf into String
+        let mut discovered_layouts: Vec<String> = available_layouts(base_path)
+            .unwrap()
+            .into_iter()
+            .map(|path| path.to_str().unwrap().to_string())
+            .collect();
+
+        // sort them, just to be sure
+        discovered_layouts.sort_by_key(|a| a.to_lowercase());
+        expected_path_list.sort_by_key(|a| a.to_lowercase());
+
+        assert_eq!(discovered_layouts, expected_path_list);
     }
 }
 
