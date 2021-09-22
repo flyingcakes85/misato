626f63700a7b268e609203f384774ba3a2ba8525
Snehit Sah <snehitsah@protonmail.com>
2021-09-22 20:26:03 +0530

    Remove old build code

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,25 @@
 
 use std::env;
 
-mod build;
 mod init;
 mod new_build;
 
 fn main() {
-    new_build::build();
-    // let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    // if args.len() > 1 {
-    //     if args[1] == "init" {
-    //         init::init_project(env::current_dir().unwrap());
-    //     } else if args[1] == "new" {
-    //         if args.len() > 2 {
-    //             init::create_project(args[2].clone(), env::current_dir().unwrap());
-    //         } else {
-    //             eprintln!("[ERR] Please provide a name for this project.")
-    //         };
-    //     } else if args[1] == "build" {
-    //         build::build_project();
-    //     } else {
-    //         eprintln!("[ERR] Subcommand \"{}\" not recognized.", args[1]);
-    //     }
-    // }
+    if args.len() > 1 {
+        if args[1] == "init" {
+            init::init_project(env::current_dir().unwrap());
+        } else if args[1] == "new" {
+            if args.len() > 2 {
+                init::create_project(args[2].clone(), env::current_dir().unwrap());
+            } else {
+                eprintln!("[ERR] Please provide a name for this project.")
+            };
+        } else if args[1] == "build" {
+            new_build::build();
+        } else {
+            eprintln!("[ERR] Subcommand \"{}\" not recognized.", args[1]);
+        }
+    }
 }
